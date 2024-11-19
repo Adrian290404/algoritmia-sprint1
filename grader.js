05674f42e979abff7855567f61a09a797546a0cb
Adrian290404 <adrianmg2004.29@gmail.com>
2024-11-19 17:54:24 +0100

    grader

---
+++ grader.js
@@ -0,0 +1,14 @@
+function grader(score) {
+    switch(true){
+        case (score <= 1 && score >= 0.9):
+            return 'A'
+        case (score < 0.9 && score >= 0.8):
+            return 'B'
+        case (score < 0.8 && score >= 0.7):
+            return 'C'
+        case (score < 0.7 && score >= 0.6):
+            return 'D'
+        default:
+            return 'F'
+    }
+}
